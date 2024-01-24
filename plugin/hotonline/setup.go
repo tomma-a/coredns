11b7630815c650146c52b58967c2148950c7978c
tomma <32663244+tomma-a@users.noreply.github.com>
2024-01-24 21:20:22 +0800

    strict test on conneciton for github

--- plugin/hotonline/setup.go
+++ plugin/hotonline/setup.go
@@ -144,13 +144,14 @@ func test_github_connection(ip string, domain string) bool {
 	req.Header.Set("Host", domain)
 	req.Header.Set("user-agent", "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36")
 	resp, err := client.Do(req)
-	if err == nil {
+	if err == nil && resp.StatusCode==200 {
 		defer resp.Body.Close()
 		return true
 	}
-	if strings.Index(err.Error(),"wsarecv")>0 {
+	/*if strings.Index(err.Error(),"wsarecv")>0 {
 		return true   // tls sni problem
 	  }
+	  */
 	fmt.Println(err)
 
 	return false
